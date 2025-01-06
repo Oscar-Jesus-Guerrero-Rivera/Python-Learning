e982a462d95e422a011fb2b36835da1e7f4fd674
Oscar Guerrero <guerreroriveraoscar@outlook.com>
2025-01-06 09:10:16 -0500

    Add count_words function

--- Learning_projects/Book_Bot/main.py
+++ Learning_projects/Book_Bot/main.py
@@ -1,12 +1,27 @@
 def main():
-    path_to_file = "Books/frankenstein.txt"
+    path_to_file = "c:/Users/cammy/.vscode/Backend_y_Frontend/Python/Learning_projects/Book_Bot/Books/frankenstein.txt"
 
     try:
-        with open(path_to_file) as f:
+        with open(path_to_file, "r") as f:
             file_contents = f.read()
-        print(file_contents)
-
+        return file_contents
+    
     except FileNotFoundError:
         print(f"Error: The file '{path_to_file}' was not found.")
+        return None
     except Exception as e:
-        print(f"An error occurred: {e}") 
+        print(f"An error occurred: {e}")
+        return None
+    
+def count_words():
+    book_text = main()
+    if book_text is None:
+        print("No text to process. Exiting.")
+        return
+    
+    word_list = book_text.split()
+    word_number = len(word_list)
+    print(f"The total number of words is: {word_number}")
+
+
+count_words()
