e883411572ec9dc4454af3bea06fc19459c23e95
Oscar Guerrero <guerreroriveraoscar@outlook.com>
2025-01-09 09:38:40 -0500

    Add gameloop logi

--- Learning_projects/Asteroids/venv/main.py
+++ Learning_projects/Asteroids/venv/main.py
@@ -11,13 +11,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     is_running = True
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Add other game logic or setup
-    while is_running == True:
+    while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         pygame.display.flip() 
+        dt = clock.tick(60)/1000
     
     
 
