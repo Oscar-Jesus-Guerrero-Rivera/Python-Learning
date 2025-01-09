57932f3abcd52e0f2a33ba2f379687db4943c060
Oscar Guerrero <guerreroriveraoscar@outlook.com>
2025-01-09 10:45:47 -0500

    Add player update method

--- Learning_projects/Asteroids/venv/main.py
+++ Learning_projects/Asteroids/venv/main.py
@@ -20,7 +20,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        player_instance.update(dt)
         player_instance.draw(screen)
         pygame.display.flip() 
         dt = clock.tick(60)/1000
