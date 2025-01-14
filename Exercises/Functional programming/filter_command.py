4c1a34eb9951f9eeec9fd6f36f8b42cbfdf3b818
Oscar Guerrero <guerreroriveraoscar@outlook.com>
2025-01-14 08:24:16 -0500

    Add functional programming exercise, filter command

---
+++ Exercises/Functional programming/filter_command.py
@@ -0,0 +1,169 @@
+"""
+Assignment
+Complete the get_filter_cmd function. It takes two functions as input, filter_one and filter_two,
+ and returns a function, filter_cmd.
+
+filter_cmd should take as input:
+
+a string content to be filtered
+an option with a default value of --one.
+The filter_cmd should filter and return the content according to the input option. 
+Do not use the builtin filter function.
+
+If --one, use filter_one
+If --two, use filter_two
+If --three, use filter_one first, then use filter_two
+If another option is passed, raise an exception, "invalid option"
+"""
+
+def get_filter_cmd(filter_one, filter_two):
+    def filter_cmd(content, option = "--one"):
+        if option == "--one":
+            return filter_one(content)
+        if option == "--two":
+            return filter_two(content)
+        if option == "--three":
+            return filter_two(filter_one(content))
+        else:
+            raise Exception ("invalid option")
+    return filter_cmd
+
+
+# don't touch below this line
+
+
+def replace_bad(text):
+    return text.replace("bad", "good")
+
+
+def replace_ellipsis(text):
+    return text.replace("..", "...")
+
+
+def fix_ellipsis(text):
+    return text.replace("....", "...")
+
+
+run_cases = [
+    (
+        replace_bad,
+        replace_ellipsis,
+        [
+            (
+                (
+                    "I'm bad, and that's good. I will never be good, and that's not bad..",
+                ),
+                "I'm good, and that's good. I will never be good, and that's not good..",
+            ),
+            (
+                (
+                    "I'm bad, and that's good. I will never be good, and that's not bad..",
+                    "--one",
+                ),
+                "I'm good, and that's good. I will never be good, and that's not good..",
+            ),
+            (
+                (
+                    "I'm bad, and that's good. I will never be good, and that's not bad..",
+                    "--two",
+                ),
+                "I'm bad, and that's good. I will never be good, and that's not bad...",
+            ),
+            (
+                (
+                    "I'm bad, and that's good. I will never be good, and that's not bad..",
+                    "--three",
+                ),
+                "I'm good, and that's good. I will never be good, and that's not good...",
+            ),
+        ],
+    ),
+]
+
+submit_cases = run_cases + [
+    (
+        replace_ellipsis,
+        fix_ellipsis,
+        [
+            (
+                (
+                    "There's no place like home.. but sometimes, it's nice to get away... and explore....",
+                ),
+                "There's no place like home... but sometimes, it's nice to get away.... and explore......",
+            ),
+            (
+                (
+                    "There's no place like home.. but sometimes, it's nice to get away... and explore....",
+                    "--one",
+                ),
+                "There's no place like home... but sometimes, it's nice to get away.... and explore......",
+            ),
+            (
+                (
+                    "There's no place like home.. but sometimes, it's nice to get away... and explore....",
+                    "--two",
+                ),
+                "There's no place like home.. but sometimes, it's nice to get away... and explore...",
+            ),
+            (
+                (
+                    "There's no place like home.. but sometimes, it's nice to get away... and explore....",
+                    "--three",
+                ),
+                "There's no place like home... but sometimes, it's nice to get away... and explore.....",
+            ),
+            (
+                (
+                    "There's no place like home.. but sometimes, it's nice to get away... and explore....",
+                    "",
+                ),
+                "invalid option",
+            ),
+        ],
+    ),
+]
+
+
+def test(filter_one, filter_two, test_cases):
+    print("---------------------------------")
+    print(f"Input functions: {filter_one.__name__} and {filter_two.__name__}")
+    filter_cmd = get_filter_cmd(filter_one, filter_two)
+    failed = False
+    for case in test_cases:
+        try:
+            result = filter_cmd(*case[0])
+        except Exception as e:
+            result = str(e)
+        expected_output = case[1]
+        print(f"Expecting: {expected_output}")
+        print(f"   Actual: {result}")
+        if result != expected_output:
+            failed = True
+            print("Fail")
+        else:
+            print("Pass")
+    passed = not failed
+    return passed
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
