3057a3b6b8f462483740b62c000b7a4a600e8cb5
Oscar Guerrero <guerreroriveraoscar@outlook.com>
2025-07-05 13:03:15 -0500

    AI Agent now reads the user input via sys.argv

--- Learning_projects/AI_Agent/main.py
+++ Learning_projects/AI_Agent/main.py
@@ -1,12 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
+from google import genai
+
+if len(sys.argv) <= 1 :
+    print("Error 1: No prompt specified")
+    sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
-def main():
-    print("Hello from ai-agent!")
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents= sys.argv
+)
+print(response.text)
 
+usage = response.usage_metadata
 
-if __name__ == "__main__":
-    main()
+print(f"Prompt tokens: {usage.prompt_token_count}")
+print(f"Response tokens: {usage.candidates_token_count}")
